4b39a59170b2fc74233eeb7969041ea6b57ca57b
Sam Connolly <samconnolly94@gmail.com>
2016-11-11 16:35:28 +0000

    Added wireframe mode in comments.

--- Prio Engine/Engine/D3D11.cpp
+++ Prio Engine/Engine/D3D11.cpp
@@ -627,6 +627,7 @@ bool CD3D11::InitRasterizer(D3D11_RASTERIZER_DESC& rasterDesc)
 	rasterDesc.DepthBiasClamp = 0.0f;
 	rasterDesc.DepthClipEnable = true;
 	rasterDesc.FillMode = D3D11_FILL_SOLID;
+	//rasterDesc.FillMode = D3D11_FILL_WIREFRAME
 	rasterDesc.FrontCounterClockwise = false;
 	rasterDesc.MultisampleEnable = false;
 	rasterDesc.ScissorEnable = false;
